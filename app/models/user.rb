37677c0c0ec876813ccd8327e8663496dd719f63
claire schults <claire.schults@gmail.com>
2017-06-01 11:25:47 +0200

    validations in model user

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,15 @@ class User < ApplicationRecord
   has_many :documents, through: :divorces
   has_one :divorce
   # has_many :logements
-  #   validates :gender, acceptance: { accept: ["Homme", "Femme"] }
-  #   validates :children_number, acceptance: { accept: [1..15] }
-  #   validates :status_pro, acceptance: { accept: ["Salarié", "Chef d'entreprise", "Sans Emploi", "Libéral"] }
-  #   validates :account_number, acceptance: { accept: [1..10] }
+    validates :gender, presence: true, acceptance: { accept: ["Homme", "Femme"] }
+    validates :child_nb, presence: true, acceptance: { accept: [1..15] }
+    validates :status_pro, acceptance: { accept: ["Salarié", "Chef d'entreprise", "Sans Emploi", "Libéral"] }
+    validates :bank_account_nb, presence: true, acceptance: { accept: [1..10] }
+    validates :email, :property_nb, :status_pro, :credit_nb, :insurance_nb, :vehicle_nb, :first_name, :last_name, :amiable, :contract_type, :statut_pro_conjoint, :mariage_contract, :presence true
 
 end
+
+
+
+
+
